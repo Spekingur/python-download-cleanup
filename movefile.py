6c4320c6d0ab768001ed68db38df0df79c8c4dd9
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2018-04-07 23:03:19 +0000

    first version of file detection

--- movefile.py
+++ movefile.py
@@ -1,4 +1,4 @@
-import argparse, shutil
+import argparse, shutil, re, os
 
 parser = argparse.ArgumentParser(
     description='Moves files and folders to a new location.')
@@ -12,4 +12,16 @@ args = parser.parse_args()
 src = args.paths[0]
 dest = args.paths[1]
 
-shutil.move(src, dest)
+video_pattern = "(.mp4|.avi|.mkv|.wmv|.flv)$"
+
+for root, dirs, files in os.walk(src):
+    for name in files:
+        name = str(name)
+        path_from = os.path.realpath(os.path.join(root, name))
+        path_to = os.path.realpath(os.path.join(dest, name))
+        pf_lower = path_from.lower()
+        if re.search(video_pattern, pf_lower):
+            if re.search('s[0-9]{2}', pf_lower) or re.search(
+                    'season( )*[0-9]+', pf_lower):
+                shutil.move(path_from, path_to)
+                #shutil.copytree(path_from, path_to)
